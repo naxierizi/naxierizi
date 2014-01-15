bceec29c85c1d58c24fd40a2564e492acda419b9
Tom Chen <chenniantao@gmail.com>
2014-01-15 20:38:50 +0800

    new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Naxierizi::Application.config.secret_token = '201df60b8af96bec986aaefa5a37f8371c54e48fda2253d576618a054480bd51c2f70e8d33ac52880a45a3cc880e5fe2f9386551d9b15f3fba7de2583c66ac28'
